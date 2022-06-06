c824dcb72b3ef8b196262658a239d9ec6e9d3ff0
DhanashriNerkar <dhanashrinerkar2@gmail.com>
2022-06-07 00:29:23 +0530

    Employee payroll class with new attributes

---
+++ EmployeePayroll.js
@@ -0,0 +1,78 @@
+/*Day44 UC 1 – Employee Payroll Class with New Attributes*/
+
+class EmployeePayrollData{
+	//getter and setter method
+	get id(){
+		return this._id;
+	}
+	set id(id){
+		this._id=id;
+	}
+
+	get name(){
+		return this._name;
+	}
+	set name(name){
+		let nameRegex=RegExp('^[A-Z]{1}[a-zA-Z\\s]{2,}$')
+		if (nameRegex.test(name))
+			this._name=name;
+		else
+			throw 'Name is Incorrect!';
+	}
+
+	get profilePic(){
+		return this._profilePic;
+	}
+	set profilePic(profilePic){
+		this._profilePic=profilePic;
+	}
+
+	get gender(){
+		return this._gender;
+	}
+	set gender(gender){
+		this._gender=gender;
+	}
+
+	get department(){
+		return this._department;
+	}
+	set department(department){
+		this._department=department;
+	}
+
+	get salary(){
+		return this._salary;
+	}
+	set salary(salary){
+		this._salary
+	}
+
+	get note(){
+		return this._note;
+	}
+	set note(note){
+		this._note=note;
+	}
+
+	get startDate(){
+		return this._startDate;
+	}
+	set startDate(startDate) {
+        let difference = Date.now() - startDate;
+        difference = Math.ceil(difference / (1000 * 60 * 60 * 24));
+        if (difference > 30 || difference < 0) {
+          throw "Start Date is Invalid";
+        } else {
+          this._startDate = startDate;
+        }
+    }
+
+	//method
+	toString(){
+		const options={year:'numeric',month:'long',day:'numeric'};
+		const empDate=!this._startDate?"undefined": this.startDate.toLocalDateString("en-US",options);
+		return "id="+this.id+",name="+this.name+",gender="+this.gender+",profilePic="+profilePic+",department="+this.department+",salary="+this.salary+
+		         ",startDate="+empDate+",note="+this.note;
+	}
+}
