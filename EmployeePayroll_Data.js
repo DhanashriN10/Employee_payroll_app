cf18c150630f9b992fa5fdf1c932f4886b9ad728
DhanashriNerkar <dhanashrinerkar2@gmail.com>
2022-06-06 22:39:36 +0530

    UC9_On form submit populate employee payroll data object

---
+++ EmployeePayroll_Data.js
@@ -0,0 +1,13 @@
+//  UC 9 -  On Form Submit populate the Employee Payroll Data 
+
+class EmployeePayrollData {
+
+    constructor(...params) {
+        this.name = params[0];
+        this.profileImage = params[1];
+        this.gender = params[2];
+        this.department = params[3];
+        this.salary = params[4];
+        this.startDate = params[5];
+        this.notes = params[6];
+    }
