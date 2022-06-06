9d864a6691bf9ff7df0316768521b28f997245fb
DhanashriNerkar <dhanashrinerkar2@gmail.com>
2022-06-07 01:49:06 +0530

    On Save Create Employee Payroll Object

--- EmployeePayroll_Data.js
+++ EmployeePayroll_Data.js
@@ -86,11 +86,11 @@ class EmployeePayrollData1 {
     }
 
 
-    toString() {
-        const options = { year: 'numeric', month: 'long', day: 'numeric' };
-        const employeeDate = this.startDate == undefined ? "undefined" : this.startDate.toLocaleDateString("en-us", options);
-        return "Name = " + this.name + ", Profile Image = " + this.profileImage + ", Gender = " + this.gender + ", Department = " + this.department + ", Salary = " + this.salary + ", Start Date = " + employeeDate + ", Notes = " + this.notes;
-    }
+   // toString() {
+     //   const options = { year: 'numeric', month: 'long', day: 'numeric' };
+       // const employeeDate = this.startDate == undefined ? "undefined" : this.startDate.toLocaleDateString("en-us", options);
+        //return "Name = " + this.name + ", Profile Image = " + this.profileImage + ", Gender = " + this.gender + ", Department = " + this.department + ", Salary = " + this.salary + ", Start Date = " + employeeDate + ", Notes = " + this.notes;
+    //}
 }
 
 
